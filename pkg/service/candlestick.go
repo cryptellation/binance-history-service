ec0d988ef0a16baae6d700eb87e6f6ae55839606
Louis FRADIN <louis.fradin@gmail.com>
2021-02-22 20:21:11 +0100

    pkg/service: add candlestick mock do operation

--- pkg/service/candlestick.go
+++ pkg/service/candlestick.go
@@ -1,11 +1,15 @@
 package service
 
 import (
+	"context"
+
 	binance "github.com/adshao/go-binance/v2"
+	"github.com/cryptellation/models.go"
 )
 
 // CandleStickServiceInterface is the interface for candle stick services
 type CandleStickServiceInterface interface {
+	Do(ctx context.Context) ([]models.CandleStick, error)
 }
 
 // CandleStickService is the real service for candlesticks
@@ -13,6 +17,32 @@ type CandleStickService struct {
 	service *binance.KlinesService
 }
 
+// Do will execute a request for candlesticks
+func (s *CandleStickService) Do(ctx context.Context) ([]models.CandleStick, error) {
+	// TODO
+	return nil, nil
+}
+
+// Mock section
+////////////////////////////////////////////////////////////////////////////////
+
+type testCandleSticks struct {
+	Exchange     string
+	Symbol       string
+	Period       int64
+	CandleSticks []models.CandleStick
+}
+
 // MockedCandleStickService is the mocked service for candlesticks
 type MockedCandleStickService struct {
+	TestCandleSticks []testCandleSticks
+}
+
+// Do will execute a request for candlesticks
+func (m *MockedCandleStickService) Do(ctx context.Context) ([]models.CandleStick, error) {
+	cs := make([]models.CandleStick, 0)
+	for _, t := range m.TestCandleSticks {
+		cs = append(cs, t.CandleSticks...)
+	}
+	return cs, nil
 }
--- pkg/service/candlestick_test.go
+++ pkg/service/candlestick_test.go
@@ -1,1 +1,33 @@
 package service
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/cryptellation/models.go"
+)
+
+var tCS = []testCandleSticks{
+	{
+		"binance", "BTC-USDC", models.M1, []models.CandleStick{
+			{time.Time{}, 10, 10, 10, 10},
+			{time.Time{}, 10, 10, 10, 10}},
+	},
+	{
+		"coinbase", "ETH-USDC", models.M5, []models.CandleStick{
+			{time.Time{}, 20, 20, 20, 20},
+			{time.Time{}, 20, 20, 20, 20}},
+	},
+}
+
+func TestMockedDo(t *testing.T) {
+	// Get new service
+	s := MockedCandleStickService{tCS}
+
+	// Do the service
+	cs, _ := s.Do(context.Background())
+	if len(cs) != 4 {
+		t.Error("There should be 4 candlesticks")
+	}
+}
