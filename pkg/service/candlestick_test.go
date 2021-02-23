3966d4c1f7c9f611ed6e49d8557132e30459d4b9
Louis FRADIN <louis.fradin@gmail.com>
2021-02-23 07:06:23 +0100

    pkg/service: add candlestick test service

--- pkg/service/candlestick_test.go
+++ pkg/service/candlestick_test.go
@@ -11,13 +11,13 @@ import (
 var tCS = []testCandleSticks{
 	{
 		"binance", "BTC-USDC", models.M1, []models.CandleStick{
-			{time.Time{}, 10, 10, 10, 10},
-			{time.Time{}, 10, 10, 10, 10}},
+			{Time: time.Time{}, Open: 10, High: 10, Low: 10, Close: 10},
+			{Time: time.Time{}, Open: 15, High: 15, Low: 15, Close: 15}},
 	},
 	{
 		"coinbase", "ETH-USDC", models.M5, []models.CandleStick{
-			{time.Time{}, 20, 20, 20, 20},
-			{time.Time{}, 20, 20, 20, 20}},
+			{Time: time.Time{}, Open: 20, High: 20, Low: 20, Close: 20},
+			{Time: time.Time{}, Open: 25, High: 25, Low: 25, Close: 25}},
 	},
 }
 
@@ -29,5 +29,19 @@ func TestMockedDo(t *testing.T) {
 	cs, _ := s.Do(context.Background())
 	if len(cs) != 4 {
 		t.Error("There should be 4 candlesticks")
+	}
+
+	// Test first case
+	for i, c := range tCS[0].CandleSticks {
+		if c != cs[i] {
+			t.Error("Candlesticks", i, "don't correspond")
+		}
+	}
+
+	// Test second case
+	for i, c := range tCS[1].CandleSticks {
+		if c != cs[i+2] {
+			t.Error("Candlesticks", i, "don't correspond")
+		}
 	}
 }
